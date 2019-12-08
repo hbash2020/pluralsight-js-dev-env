50d591dfb08d5369dc2c08749e608efba46cba0d
hbash2020 <54945359+hbash2020@users.noreply.github.com>
2019-12-08 12:42:25 -0500

    Sunday Updates

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,19 +2,18 @@ var express = require('express');
 var path = require('path');
 var open = require('open');
 
-var port = 14567
+var port = 14567;
 
 var app = express();
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+	res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-
 app.listen(port, function(err) {
-  if (err) {
-    console.log(err);
-  }  else {
-    open('http://localhost:' + port);
-  }
+	if (err) {
+		console.log(err);
+	} else {
+		open('http://localhost:' + port);
+	}
 });
